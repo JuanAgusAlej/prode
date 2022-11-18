386f1b7819582a4ea5a1fc48c98213254ccc6880
ElianRivoira <eli_rivoira@hotmail.com>
2022-11-18 13:09:38 -0300

    useEffect confirmRegister

--- src/pages/ConfirmRegister/ConfirmRegister.jsx
+++ src/pages/ConfirmRegister/ConfirmRegister.jsx
@@ -1,8 +1,11 @@
 import { React, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const ConfirmRegister = () => {
+  // useParams()
   useEffect(() => {
     //  axios to the route for validate the user
+    // axios
     console.log('validate user');
   }, []);
   return <div>Your register has been successfully completed</div>;
