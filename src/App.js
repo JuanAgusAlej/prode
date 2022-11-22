cf644e4fdb26d07d9b88746750b1d94b5f6c8700
Agustinsa-hub <agustinsa1999@gmail.com>
2022-11-22 16:02:10 -0300

    profile page basico

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './App.css';
 import LoginPages from './pages/Login/LoginPages.jsx';
+import ProfilePages from './pages/Profile/ProfilePages.jsx';
+import ProfileEditorPages from './pages/Profile/ProfileEditor/ProfileEditorPages.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" index element={<LoginPages />} />
+        <Route path="/profile" index element={<ProfilePages />} />
+        <Route path="/profile/edit" index element={<ProfileEditorPages />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/utils/useInput.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function useInput() {
+  const [value, setValue] = useState('');
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return { value, onChange };
+}
+
+export default useInput;
