4cc586c57ef02f06e535113ece4cfd19d3ca3f51
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2022-11-22 16:30:06 -0300

    the fixture page has been added with its respective components and styles.

---
+++ src/commons/Visitor/Visitor.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './visitor.css';
+
+const Visitor = ({
+  gol, team, local, win,
+}) => {
+  return (
+    <>
+      {!local ? (
+        <div
+          className={
+            win === team.name || win === ''
+              ? 'd-flex align-items-center '
+              : 'd-flex align-items-center lose'
+          }>
+          <p className="mx-2 my-2">{gol}</p>
+          <p className="me-2 my-2">{team.name}</p>
+          <img src={team.logo} alt="" width="40" height="34" />
+        </div>
+      ) : (
+        <div
+          className={
+            win === team.name || win === ''
+              ? 'd-flex align-items-center '
+              : 'd-flex align-items-center lose'
+          }>
+          <img src={team.logo} alt="" width="40" height="34" />
+          <p className="ms-2 my-2 ">{team.name}</p>
+          <p className="mx-2 my-2">{gol}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Visitor;
