37e97fd49b0a291266af9c378d90528755cf0701
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2022-11-24 14:21:30 -0300

    linkage with the back

--- src/state/tournament.js
+++ src/state/tournament.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const initialState = {
-  isLoading: false,
+  isLoading: true,
   tournament: null,
 };
 
