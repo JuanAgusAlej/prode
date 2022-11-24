bebd251510f7e01f0d7c0f2a49933357a149f773
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2022-11-24 12:00:15 -0300

    routing configuration

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -1,18 +1,41 @@
-import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Link, useLocation } from 'react-router-dom';
 import './menuBar.css';
 
 const MenuBar = () => {
   const location = useLocation();
-  if (location.pathname === '/' || location.pathname === '/validation') {
+  const pathname = location.pathname;
+  const { user } = useSelector((state) => state);
+  if (pathname === '/' || pathname === '/validation') {
     return;
   }
 
   return (
     <nav className="navbar fixed-bottom navbar-dark bg-dark">
       <div className="container-fluid my-1 align-items-center mx-2 icon ">
-        <i className="bi bi-person "></i>
-        <i className="bi bi-house "></i>
-        <i className="bi bi-controller "></i>
+        <Link to={`/profile/${user.userData?.id}`}>
+          <i
+            className={
+              pathname === `/profile/${user.userData?.id}`
+                ? 'bi bi-person active'
+                : 'bi bi-person'
+            }></i>
+        </Link>
+
+        <Link to={'/home'}>
+          <i
+            className={
+              pathname === '/home' ? 'bi bi-house active' : 'bi bi-house'
+            }></i>
+        </Link>
+        <Link to={'/leaderboard'}>
+          <i
+            className={
+              pathname === '/leaderboard'
+                ? 'bi bi-controller active'
+                : 'bi bi-controller '
+            }></i>
+        </Link>
       </div>
     </nav>
   );
