9b13fa9ddc0b9d2ea4389572133c440a8c242585
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2022-11-24 18:31:57 -0300

    visitor style modification

--- src/commons/Visitor/Visitor.jsx
+++ src/commons/Visitor/Visitor.jsx
@@ -9,23 +9,23 @@ const Visitor = ({
       {!local ? (
         <div
           className={
-            win === 'WON_B' || win === 'PENDING'
+            win === 'WON_B' || win === 'PENDING' || win === 'DRAW'
               ? 'd-flex align-items-center '
               : 'd-flex align-items-center lose'
           }>
           <p className="mx-2 my-2">{gol}</p>
-          <p className="me-2 my-2">{team.name}</p>
+          <p className="me-2 my-2">{team.shortName}</p>
           <img src={team.logo} alt="" width="40" height="34" />
         </div>
       ) : (
         <div
           className={
-            win === 'WON_A' || win === 'PENDING'
+            win === 'WON_A' || win === 'PENDING' || win === 'DRAW'
               ? 'd-flex align-items-center '
               : 'd-flex align-items-center lose'
           }>
           <img src={team.logo} alt="" width="40" height="34" />
-          <p className="ms-2 my-2 ">{team.name}</p>
+          <p className="ms-2 my-2 ">{team.shortName}</p>
           <p className="mx-2 my-2">{gol}</p>
         </div>
       )}
