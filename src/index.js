b6f88db7ae4b00ef1c653aa3705812bad33d8b82
lucianocasini <lucianoo.casini@gmail.com>
2022-12-02 09:56:47 -0300

    Fix push notifications

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './utils/i18n';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import { registerServiceWorker } from './service-worker';
 import store from './state/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,12 +14,4 @@ root.render(
   </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
